c37469a789a9d1d9af4b2dce23b213fde64df064
Ananism17 <anandiljar5@gmail.com>
2024-02-28 11:53:39 +0600

    Initial Commit with User APIs

---
+++ server.js
@@ -0,0 +1,47 @@
+import express from "express";
+import dotenv from "dotenv";
+
+//routes
+import userRoutes from "./routes/userRoutes.js";
+
+//middlewares
+import { notFound, errorHandler } from "./middlewares/errorMiddleware.js";
+
+//configs
+import connectDB from "./config/db.js";
+
+//cookie-parser
+import cookieParser from "cookie-parser";
+
+//env-config
+dotenv.config();
+
+//connect-db
+connectDB();
+
+//init-app-port
+const app = express();
+const port = process.env.PORT || 5555;
+
+//middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+//routes
+app.use("/api/users", userRoutes);
+
+//error-middlewares
+app.use(notFound);
+app.use(errorHandler);
+
+//start-server
+app.listen(port, () => {
+  console.log("Listening on port", port);
+});
